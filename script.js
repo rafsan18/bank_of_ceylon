1092d9242046a20e85fbda6c0e8fe6a27d2073d7
rafsan <rafsanamin18@gmail.com>
2020-07-23 18:45:24 +0600

    vanished login area on click

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById("loginBtn");
+loginBtn.addEventListener("click", function () {
+  const loginArea = document.getElementById("login-area");
+  loginArea.style.display = "none";
+});
