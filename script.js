125c12f526dc7b0ba2680045d5bb558e24565a70
rafsan <rafsanamin18@gmail.com>
2020-07-23 20:55:58 +0600

    deposit event handler added

--- script.js
+++ script.js
@@ -6,3 +6,25 @@ loginBtn.addEventListener("click", function () {
   const transactionArea = document.getElementById("transaction-area");
   transactionArea.style.display = "block";
 });
+
+// Deposit Event handler
+const depositBtn = document.getElementById("depositBtn");
+depositBtn.addEventListener("click", function () {
+  const depositAmount = document.getElementById("depositAmount").value;
+  const depositAmountNumber = parseFloat(depositAmount);
+
+  const currentDeposit = document.getElementById("currentDeposit").innerText;
+  const currentDepositNumber = parseFloat(currentDeposit);
+
+  const depositBalance = depositAmountNumber + currentDepositNumber;
+
+  document.getElementById("currentDeposit").innerText = depositBalance;
+
+  //   Adjusting Balance
+  const currentBalance = document.getElementById("currentBalance").innerText;
+  const currentBalanceNumber = parseFloat(currentBalance);
+  const updatedBalance = depositAmountNumber + currentBalanceNumber;
+  document.getElementById("currentBalance").innerText = updatedBalance;
+
+  document.getElementById("depositAmount").value = "";
+});
