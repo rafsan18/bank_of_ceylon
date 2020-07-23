4373f3aed2911948ce83c151ebfc0385ef92dec3
rafsan <rafsanamin18@gmail.com>
2020-07-23 19:59:15 +0600

    transaction page style done

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+// login page to transaction page
 const loginBtn = document.getElementById("loginBtn");
 loginBtn.addEventListener("click", function () {
   const loginArea = document.getElementById("login-area");
   loginArea.style.display = "none";
+  const transactionArea = document.getElementById("transaction-area");
+  transactionArea.style.display = "block";
 });
